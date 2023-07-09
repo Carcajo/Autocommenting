2ba0cadba751dda7b237b990cf4c74a0bc542f42
Maxim Puhov <93794796+Carcajo@users.noreply.github.com>
2023-07-09 09:35:46 +0300

    add private_data.py

---
+++ private_data.py
@@ -0,0 +1,3 @@
+api_id = 'your api_id'                                              #int
+api_hash = 'your api_hash'                                          #str
+debug_chat_id = 'id of the chat where the errors will be sent to'   #int
