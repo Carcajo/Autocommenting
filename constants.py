be3636cd9d7afb6a247698f0e2343bc55f6bf581
Maxim Puhov <93794796+Carcajo@users.noreply.github.com>
2023-07-10 19:23:14 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 #all in str
 
-API_KEY = ''
-URL = ''
-SHEET_NAME = 'Макс_список_каналов'
-SHEET_NAME_DATA = 'Макс'
-SERVICE_FILE = ''
+API_KEY = 'key from ChatGPT'
+URL = 'link to the Google Table'
+SHEET_NAME = 'list channels that we monitoring '
+SHEET_NAME_DATA = 'Google Table with information about program working'
+SERVICE_FILE = 'json file for access to Google Sheets'
